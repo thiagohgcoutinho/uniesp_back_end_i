19a550b6526eea6433818e6458a039bab082a592
Thiago Coutinho <2022211510098@iesp.edu.br>
2023-10-01 12:54:44 -0300

    AulaRemota

--- AlunoOnlineV2/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
+++ AlunoOnlineV2/api/src/main/java/com/alunoonline/api/controller/AlunoController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 // Identificação da classe Controller no Spring com a arquitetura REST
 @RestController
 // Mapeamento das requisições do FrontEnd
@@ -26,5 +29,39 @@ public class AlunoController {
 
         return ResponseEntity.status(201).body(alunoCreated);
     }
+
+    // Requisição do tipo GET
+    @GetMapping("all")
+    // Resposta da requisição
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> findAll(){
+        return service.findAll();
+    }
+
+    // Requisição do tipo GET
+    @GetMapping("{id}")
+    // Resposta da requisição
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    // Requisição do tipo DELETE
+    @DeleteMapping("{id}")
+    // Resposta da requisição
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+
+    //requisição do tipo PATCH
+    @PatchMapping("{id}")
+    // Resposta da requisição
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Aluno> update(@PathVariable Long id, @RequestBody Aluno aluno){
+        Aluno alunoUpdate = service.update(id, aluno);
+        return ResponseEntity.status(200).body(alunoUpdate);
+    }
+
 
 }
--- AlunoOnlineV2/api/src/main/java/com/alunoonline/api/service/AlunoService.java
+++ AlunoOnlineV2/api/src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,9 @@ import com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 // Identificação do Service do Spring
 @Service
 public class AlunoService {
@@ -12,9 +15,39 @@ public class AlunoService {
     // Injeção dos métodos do repository
     @Autowired
     AlunoRepository repository;
-    
+
+    // método para criar aluno
     public Aluno create(Aluno aluno){
         return repository.save(aluno);
     }
-    
+
+    // método para buscar todos os alunos
+    public List<Aluno> findAll(){
+        return repository.findAll();
+    }
+
+    //método para buscar um aluno pelo Id
+    public Optional<Aluno> findById(Long id){
+        return repository.findById(id);
+    }
+
+    // método para deletar um aluno pelo Id
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    //método para atualizar um aluno pelo Id
+    public Aluno update(Long id, Aluno alunoUpdated){
+        Aluno aluno = repository.findById(id).get();
+        if (alunoUpdated.getNome() != null) {
+            aluno.setNome(alunoUpdated.getNome());
+        }
+        if (alunoUpdated.getCurso() != null) {
+            aluno.setCurso(alunoUpdated.getCurso());
+        }
+        if (alunoUpdated.getEmail() != null) {
+            aluno.setEmail(alunoUpdated.getEmail());
+        }
+        return repository.save(aluno);
+    }
 }
